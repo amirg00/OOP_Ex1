74d4afa57e749cd73884db0e52eab406147bd1bd
amirg00 <amirg00@walla.com>
2021-11-18 03:40:59 +0200

    few documentations added

--- src/Elevator.py
+++ src/Elevator.py
@@ -4,17 +4,30 @@ import math
 class Elevator:
 
     def __init__(self, id, speed, min_floor, max_floor, close_time, open_time, start_time, stop_time):
+        # the id of the current elevator.
         self.id = id
+        # the speed of the current elevator.
         self.speed = speed
+        # the minimum floor of the current elevator.
         self.min_floor = min_floor
+        # the maximum floor of the current elevator.
         self.max_floor = max_floor
+        # the close time of the current elevator.
         self.close_time = close_time
+        # the open time of the current elevator.
         self.open_time = open_time
+        # the start time of the current elevator.
         self.start_time = start_time
+        # the stop time of the current elevator.
         self.stop_time = stop_time
+        # the associated calls list contains all calls that have optimal time
+        # with the current elevator.
         self.associated_calls = []
+        # copy_calls = the copy of the associated calls list.
         self.copy_calls = []
+        # the time stamps list
         self.time_stamps = []
+        #
         self.time_stamps_copy = []
         self.curr_total_time = 0
 
--- src/main.py
+++ src/main.py
@@ -1,18 +1,36 @@
+"""Exercise 1 - OOP
+
+This main manages everything including the following:
+
+        - Parser. the parser gets 3 parameters: json file contains the list
+          elevators, the min and max floor of the elevator. In addition, it gets
+          a csv file contains the calls, and for the final parameter it gets a csv
+          file to have all calls with the allocated elevator column modified.
+
+        - Algorithm. the main is also responsible to run our offline algorithm
+          with the building, calls, output parser's parameters, and finally
+          update the output file.
+
+This project implements the concepts are mentioned in Github repository's README.md file.
+
+    * main - the main function of the offline scheduling algorithm.
+"""
 import argparse
-import math
 from Building import Building
 from Calls import Calls
 from Algorithm import Algorithm
 
 
 def main():
-    parser = argparse.ArgumentParser(description='offline elevator.')
-    parser.add_argument('building', help='json file that contains the building')
-    parser.add_argument('calls', help='csv file that contains the calls')
-    parser.add_argument('output', help='csv file')
+    parser = argparse.ArgumentParser(description='Ex1: offline elevator algorithm')
+    parser.add_argument('building', help='building json file')
+    parser.add_argument('calls', help=' calls csv file')
+    parser.add_argument('output', help='scheduled calls csv file')
+
     args = parser.parse_args()
     building = Building(args.building)
     calls = Calls(args.calls)
+
     algo = Algorithm(calls.copy_of_round_calls, building.elevators, calls.calls)
     algo.to_main()
     calls.allocated_calls(algo.original_calls)
