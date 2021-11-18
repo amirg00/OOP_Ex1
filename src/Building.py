e5bd8f37f936b59350cc75dcb8ca8b6fb0e79be0
amirg00 <amirg00@walla.com>
2021-11-18 14:00:10 +0200

    DOCUMENTATIONS ADDED FOR Building.py

--- src/Building.py
+++ src/Building.py
@@ -3,25 +3,48 @@
 # Author: Amir Gillette
 # Course: OOP
 # ----------------------------------------------------------------------------
+
 import json
 from Elevator import Elevator
 
 
 class Building:
     def __init__(self, file_name):
-        # file name = the name of the file.
+        """
+             The constructor initialize the file name property with the json file name, represent the building,
+             it gets. In addition, it also gets a couple properties: minFloor, maxFloor and elevator.
+             Afterwards, this constructor calls json_reader with the initialized file name property.
+             Parameters
+             ----------
+             file_name : str
+                the name of the file
+
+            Returns
+            -------
+            None
+        """
         self.file_name = file_name
-        # minFloor = the lowest floor of the building.
+        # minFloor = the lowest floor of the building that the elevator can reach.
         self.minFloor = 0
-        # minFloor = the highest floor of the building.
+        # minFloor = the highest floor of the building that the elevator can reach.
         self.maxFloor = 0
         # elevators is a list of all the elevators in the building.
         self.elevators = []
         self.json_reader(self.file_name)
 
-    # The function gets the name of the json file, save the dictionary of the json file.
-    # Then, the function goes over the dictionary and update the properties of the class.
     def json_reader(self, file_name):
+        """
+            The function gets the name of the json file, save the dictionary of the json file.
+            Then, the function goes over the dictionary and update the properties of the class.
+            Parameters
+            ----------
+            file_name : str
+                the name of the file
+
+            Returns
+            -------
+            None
+        """
         try:
             with open(file_name, "r+") as f:
                 dictionary = json.load(f)
@@ -34,8 +57,3 @@ class Building:
                                                    value["_openTime"], value["_startTime"], value["_stopTime"]))
         except IOError as e:
             print (e)
-
-    # The function returns building's number of floors.
-    def number_of_floors(self):
-        return self.maxFloor - self.minFloor
-
